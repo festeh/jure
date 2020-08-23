f54ae90213e3ef765a81e61c8fd714d2560a8080
dima <dimich3d@ya.ru>
2020-08-23 16:32:25 +0300

    move event to separate file

--- src/events.py
+++ src/events.py
@@ -5,4 +5,9 @@ class EventType(Enum):
     RELOAD_PAGE = 1
     GO_TO_CELL = 2
 
+
 reload_event = dict(type=EventType.RELOAD_PAGE)
+
+
+def scroll_event(cell_num):
+    return {"type": EventType.GO_TO_CELL, "value": cell_num}
