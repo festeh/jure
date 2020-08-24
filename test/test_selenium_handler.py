d8b2f73e7546c18c19e5c7447ee05e0166e18bb2
dima <dimich3d@ya.ru>
2020-08-24 23:30:30 +0300

    update test to not depend on layout

--- test/test_selenium_handler.py
+++ test/test_selenium_handler.py
@@ -4,12 +4,14 @@ from signal import SIGKILL
 from time import sleep
 
 import jupytext
+from selenium.webdriver.common.keys import Keys
 
 from src.events import reload_event
 from src.handlers import SeleniumHandler
 from test.utils import start_jupyter_server
 
 TEST_NOTEBOOK_PATH = Path(__file__).parent / "data/test_notebook.ipynb"
+NOTEBOOK_PAGE = "http://localhost:8814/notebooks/data/test_notebook.ipynb?token=token"
 
 
 def modify_notebook_file(path):
@@ -23,8 +25,7 @@ def modify_notebook_file(path):
 
 def test_works_alert():
     jupid = start_jupyter_server()
-    page = "http://localhost:8814/notebooks/data/test_notebook.ipynb?token=token"
-    handler = SeleniumHandler(page)
+    handler = SeleniumHandler(NOTEBOOK_PAGE)
     driver = handler.driver
     sleep(1.0)
     try:
@@ -32,8 +33,7 @@ def test_works_alert():
         cell.click()
         cell.find_element_by_tag_name("textarea").send_keys(" hello")
         modify_notebook_file("data/test_notebook.ipynb")
-        save_btn = driver.find_element_by_css_selector("[title='Save and Checkpoint']")
-        save_btn.click()
+        driver.find_element_by_tag_name("body").send_keys(Keys.CONTROL + "s")
         sleep(0.5)
         handler.handle(reload_event)
     finally:
