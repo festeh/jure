58e8eb04b9fe5ca25a64182011a68fa556f25d62
dima <dimich3d@ya.ru>
2020-09-16 21:52:01 +0300

    add port arg

--- jure/main.py
+++ jure/main.py
@@ -13,6 +13,7 @@ def parse_arguments(args):
                         help="Path to directory where the Jupyter notebook is launched")
     parser.add_argument("--notebook_path", type=Path, required=True,
                         help="Path to notebook file on disk")
+    parser.add_argument("--port", default="8888", help="Port on which Jupyter is launched")
     parser.add_argument("--token", required=True, help="Secret token to get access to Jupyter")
     parsed_args = parser.parse_args(args)
     return parsed_args
@@ -21,9 +22,10 @@ def parse_arguments(args):
 def main(args=None):
     parsed_args = parse_arguments(args)
     notebook_path = parsed_args.notebook_path
+    port = parsed_args.port
     file_path = get_file_path_from_notebook_path(notebook_path)
     assert Path(file_path).exists()
-    page = f"http://localhost:8888/notebooks/{notebook_path}?token={parsed_args.token}"
+    page = f"http://localhost:{port}/notebooks/{notebook_path}?token={parsed_args.token}"
     handler = SeleniumHandler(page)
     observer = Observer()
     event_handler = WatchdogHandler(file_path, handler)
