49e09e7e83f02da68212d7ee881dc91fe883527d
dima <dimich3d@ya.ru>
2020-10-13 22:04:29 +0300

    0.2.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,3 +8,5 @@ All notable changes to this project will be documented in this file.
 ## [0.2.1] - 2020-10-11
 * fix bug with cells index
 * less logs
+## [0.2.2] - 2020-10-13
+* simplify CellsIndex, do not skip first cell
--- jure/index_notebook_cells.py
+++ jure/index_notebook_cells.py
@@ -3,14 +3,10 @@ from bisect import bisect_left
 
 class CellsIndex:
     def __init__(self, lines):
-        cell_num = None
         self.index = []
         for line_num, line in enumerate(lines, 1):
             if line.startswith("# %%"):
-                if cell_num is None:
-                    cell_num = 0
-                else:
-                    self.index.append(line_num)
+                self.index.append(line_num)
 
     def get_cell(self, line):
         return bisect_left(self.index, line)
